09c73782b8cc4e6134f78023b2cc4bad1b960372
Unknown <mizvladimir@gmail.com>
2019-01-27 15:50:34 +0100

    Dialog

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -1,19 +1,68 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+# from hermes_python.hermes import Hermes
+
+# INTENT_HOW_ARE_YOU = "mizvol:how_are_you"
+
+
+# def main():
+#     with Hermes("localhost:1883") as h:
+#         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
+#          .start()
+
+
+# def how_are_you_callback(hermes, intent_message):
+#     session_id = intent_message.session_id
+#     response = "Awesome, dude."
+#     hermes.publish_end_session(session_id, response)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
 INTENT_HOW_ARE_YOU = "mizvol:how_are_you"
+INTENT_GOOD = "mizvol:feeling_good"
+INTENT_BAD = "mizvol:feeling_bad"
+INTENT_ALRIGHT = "mizvol:feeling_alright"
+
+INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
+         .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
+         .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
+         .subscribe_intent(INTENT_ALRIGHT, feeling_alright_callback) \
          .start()
 
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "Awesome, dude."
+    response = "Awesome, dude. You?"
+    hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
+
+
+def feeling_good_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's awesome! I'm happy to hear that."
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_bad_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "Sorry to hear that. I hope you feel better soon."
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_alright_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's cool."
     hermes.publish_end_session(session_id, response)
 
 
